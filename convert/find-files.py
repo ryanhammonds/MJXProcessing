2b19ce71b40d23e6338416cacef7ce19b093d510
evan <esk170004@utdallas.edu>
2020-02-21 13:28:08 -0600

    corrected typo

--- convert/find-files.py
+++ convert/find-files.py
@@ -2,49 +2,55 @@ import os  # interface with path
 import sys  # pass arguments command line
 import fnmatch  # compare substring filename
 
-## get par or rec file, returns file
+# get par or rec file, returns file
 
 defaultPath = "/mnt/Filbey/common/Studies/MJX_WF/examples/NL"
 
 
-def findParOrRec(path):
-    rawDataList = []
+def find_par_or_rec(path):
+    raw_data_list = []
     for roots, dirs, files in os.walk(path):
         for file in files:
             if fnmatch.fnmatch(file, "*.REC"):
                 # print(file)
-                rawDataList.append(file)
-                return rawDataList
+                raw_data_list.append(file)
+                return raw_data_list
 
 
-def findT1(path=defaultPath):
-    fileNameList = []
-    for file in findParOrRec(path):
+def find_t1(path=defaultPath):
+    file_name_list = []
+    for file in find_par_or_rec(path):
         if fnmatch.fnmatch(file, "*T1*"):
-            fileNameList.append(file)
-    return fileNameList
+            file_name_list.append(file)
+    return file_name_list
 
 
-def findDwi(path=defaultPath):
-    dwiList = []
-    for file in findParOrRec(path):
-        return dwiList
+def find_dwi(path=defaultPath):
+    dwi_list = []
+    for file in find_par_or_rec(path):
+        if fnmatch.fnmatch(file, "*dwi*"):
+            dwi_list.append(file)
+    return dwi_list
 
-    def findFmap(path=defaultPath):
-        fmapList = []
-        for file in findParOrRec(path):
-            if fnmatch.fnmatch(file, "*topup*"):
-                fmapList.append(file)
-        return fmapList
 
-    def findTask(path=defaultPath):
-        taskList = []
-        for file in findParOrRec(path):
-            if fnmatch.fnmatch(file, "*bold*"):
-                if not fnmatch.fnmatch(file, "*topup*"):
-                    taskList.append(file)
-            return taskList
-        print(findT1())
-        print(findDwi())
-        print(findFmap())
-        print(findTask())
+def find_fmap(path=defaultPath):
+    fmap_list = []
+    for file in find_par_or_rec(path):
+        if fnmatch.fnmatch(file, "*topup*"):
+            fmap_list.append(file)
+    return fmap_list
+
+
+def find_task(path=defaultPath):
+    task_list = []
+    for file in find_par_or_rec(path):
+        if fnmatch.fnmatch(file, "*bold*"):
+            if not fnmatch.fnmatch(file, "*topup*"):
+                task_list.append(file)
+        return task_list
+
+
+print(find_t1())
+print(find_dwi())
+print(find_fmap())
+print(find_task())
