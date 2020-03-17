7c1d4d1d61e4c16b9c3a9a0701706cac4522c671
RyanHammonds <ryan.hammonds@utexas.edu>
2020-03-17 16:56:57 -0500

    subject class complete. bids_query added.

--- mjxproc/utils.py
+++ mjxproc/utils.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import sys
 import os
 import re
 import glob
 import warnings
 import subprocess
+import json
 
 
 class Subject:
@@ -44,6 +44,14 @@ class Subject:
         if self.prep_path.endswith('/'):
             self.prep_path = self.prep_path[:-1]
 
+        # Check arguments
+        if not os.path.isdir(self.dir_path):
+            raise IOError(f'Raw data directory missing: {self.dir_path}')
+        if not os.path.isdir(self.bids_path):
+            os.mkdir(self.bids_path)
+        if not os.path.isdir(self.prep_path):
+            os.mkdir(self.prep_path)
+
         # Determine if NL or UTD data
         self.subj_id = re.sub(".*/", '', self.dir_path)
         if self.subj_id.startswith("sub"):
@@ -60,6 +68,7 @@ class Subject:
             elif "ses-02" in ses:
                 self.bids_complete.append("ses-02")
 
+        self.raw_sess = len(glob.glob(f'{dir_path}/*'))
         self.seqs = {'anat': [], 'func': [], 'dwi': [], 'fmap': []}
 
     def _seq_glob(self, seq, topup):
@@ -109,8 +118,8 @@ class Subject:
         """
         if self.site == 'UTD':
             # Get T1w and diffusions sequences
-            self.seqs['anat'] = self._seq_glob('MPR', topup=False)
-            self.seqs['dwi'] = self._seq_glob('DTI', topup=False)
+            self.seqs['anat'] = [self._seq_glob('MPR', topup=False)]
+            self.seqs['dwi'] = [self._seq_glob('DTI', topup=False)]
 
             # Get all funcational sequences
             seq_match = ['CAAT', 'CUE_RUN1', 'CUE_RUN2', 'NBACK', 'REST']
@@ -122,8 +131,8 @@ class Subject:
             self.seqs['fmap'] = [self._seq_glob(seq, topup=True) for seq in
                                  seq_match]
         elif self.site == 'NL':
-            self.seqs['anat'] = self._seq_glob('T1w', topup=False)
-            self.seqs['dwi'] = self._seq_glob('dwi', topup=False)
+            self.seqs['anat'] = [self._seq_glob('T1w', topup=False)]
+            self.seqs['dwi'] = [self._seq_glob('dwi', topup=False)]
             seq_match = ['CAAT', 'cue*run-1', 'cue*run-2', 'nback', 'rest']
             self.seqs['func'] = [self._seq_glob(seq, topup=False) for seq in
                                  seq_match]
@@ -149,6 +158,7 @@ class Subject:
             sess_regex = list(sess_regex.values())
         elif self.site == 'NL':
             sess_regex = ['ses-01', 'ses-02']
+
         # Make subject-level BIDS directory
         base_out = f'{self.bids_path}/{self.subj_id}'
         if not os.path.isdir(base_out):
@@ -161,9 +171,10 @@ class Subject:
         else:
             dcm2niix = f'{pwd}/dcm2niix/dcm2niix'
 
+        # Create convert cmd strings and run
         func_seqs = ['CAAT', 'CUE-RUN-01', 'CUE-RUN-02', 'NBACK', 'REST']
-        for session in ['ses-01', 'ses-02']:
-            if session not in self.bids_complete:
+        for idx, session in enumerate(['ses-01', 'ses-02']):
+            if session not in self.bids_complete and idx+1 <= self.raw_sess:
                 # Setup BIDS sub-directories
                 sess_out = f'{base_out}/{session}'
                 os.mkdir(sess_out)
@@ -180,21 +191,116 @@ class Subject:
                     elif session == 'ses-02':
                         images = [seq for seq in self.seqs[img_type] if
                                   str(sess_regex[1]) in seq]
+
                     if img_type == 'anat':
                         cmd = [f'{dcm2niix} -o {sess_out}/anat -f '
-                               f'{self.subj_id}_ses-01_T1w {img}' for img in images]
+                               f'{self.subj_id}_ses-01_T1w {img}' for img in
+                               images]
                     elif img_type == 'func':
                         cmd = [f'{dcm2niix} -o {sess_out}/func -f '
                                f'{self.subj_id}_ses-01_task-{func_seqs[idx]}_bold '
                                f'{img}' for idx, img in enumerate(images)]
                     elif img_type == 'dwi':
                         cmd = [f'{dcm2niix} -o {sess_out}/dwi -f '
-                               f'{self.subj_id}_ses-01_dwi {img}' for img in images]
+                               f'{self.subj_id}_ses-01_dwi {img}' for img in
+                               images]
                     elif img_type == 'fmap':
-                        cmd = [f'{dcm2niix} -o {sess_out}/func -f '
-                               f'{self.subj_id}_ses-01_task-{func_seqs[idx]}_epi '
-                               f'{img}' for idx, img in enumerate(images)]
+                        cmd = []
+                        for idx, img in enumerate(images):
+                            if 'DTI' not in img and'dwi' not in img:
+                                cmd.append(f'{dcm2niix} -o {sess_out}/fmap -f '
+                                           f'{self.subj_id}_ses-01_acq-'
+                                           f'{func_seqs[idx]}_epi {img}')
+                            else:
+                                cmd.append(f'{dcm2niix} -o {sess_out}/fmap -f '
+                                           f'{self.subj_id}_ses-01_acq-dwi_epi '
+                                           f'{img}')
 
-                    # Exexcute conversion commands
+                    # Execute conversion commands
                     for conv in cmd:
-                        subprocess.run(conv, shell=True)
+                        subprocess.run(conv, shell=True,
+                                       stdout=subprocess.DEVNULL,
+                                       stderr=subprocess.DEVNULL)
+
+                        # Edit fmap jsons to pair with image
+                        if img_type == 'fmap':
+                            # file name and dir path from cmd
+                            f_out = re.sub(" .*", '', re.sub(".*-f ", '', conv))
+                            d_out = re.sub(" .*", '', re.sub(".*-o ", '', conv))
+
+                            img_regex = re.sub("_.*", '', re.sub(".*acq-", '',
+                                                                 f_out))
+                            img_match = glob.glob(f'{self.bids_path}/'
+                                                  f'{self.subj_id}/'
+                                                  f'{session}/*/'
+                                                  f'*{img_regex}*.nii*')
+                            img_match = [img for img in img_match if 'fmap'
+                                         not in img][0]
+
+                            with open(f"{d_out}/{f_out}.json", 'r') as f:
+                                img_json = json.load(f)
+
+                            img_json['IntendedFor'] = img_match
+                            with open(f'{d_out}/{f_out}.json', 'w') as f:
+                                json.dump(img_json, f, indent='\t')
+
+
+def bids_query(bids_dir, subj_id, ses):
+    """Find nifti images in a BIDS structure.
+    Arguments
+    ---------
+    bids_dir : str
+        path to bids directory
+    subj_id : str
+        subject id
+    ses : str
+        session number
+
+    Returns
+    -------
+    anat : list
+        list of anatomical images
+    func : list
+        list of functional images
+    dwi : list
+        list of diffusion images
+    fmap : list
+        list of field map images
+    """
+    base_dir = f'{bids_dir}/{subj_id}/{ses}'
+    anat = glob.glob(f'{base_dir}/anat/*nii*')
+    anat.sort()
+    func = glob.glob(f'{base_dir}/func/*nii*')
+    func.sort()
+    dwi = glob.glob(f'{base_dir}/dwi/*nii*')
+    dwi.sort()
+    fmap = glob.glob(f'{base_dir}/fmap/*nii*')
+    fmap.sort()
+
+    # Warn if image not found
+    if len(anat) > 1:
+        warnings.warn("Multiple anat images found.")
+    if len(dwi) > 1:
+        warnings.warn("Multiple dwi images found.")
+
+    def _check_seqs(seqs, images, type):
+        for seq in func_seqs:
+            miss_warn = True
+            multi_warn = False
+            for img in func:
+                if seq in img:
+                    miss_warn = False
+                if not multi_warn and not miss_warn:
+                    multi_warn = True
+
+            if miss_warn:
+                warnings.warn(f"No {type} image found for {seq}.")
+            elif multi_warn:
+                warnings.warn(f"Multiple {type} images found for {seq}")
+
+    func_seqs = ['CAAT', 'CUE-RUN-01', 'CUE-RUN-02', 'NBACK', 'REST']
+    _check_seqs(func_seqs, func, 'func')
+    fmap_seqs = ['CAAT', 'CUE-RUN-01', 'CUE-RUN-02', 'NBACK', 'REST', 'dwi']
+    _check_seqs(fmap_seqs, fmap, 'fmap')
+
+    return anat, func, dwi, fmap
